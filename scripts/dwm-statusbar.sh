86df50009e64c9b6860ef40eb0b66b1ac0c845b4
Donocean <1184427366@qq.com>
2022-09-07 18:45:01 +0800

    add memfree scripts

--- scripts/dwm-statusbar.sh
+++ scripts/dwm-statusbar.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+print_mem()
+{
+	MEMFREE=$(free -m | awk 'NR==2 {print $7}')
+	echo " ${MEMFREE}M"
+}
 
 printf_alsa()
 {
@@ -9,12 +14,13 @@ printf_alsa()
 	then
 		echo "婢" 
 	else
-		printf "墳%s%%" $VOL
+		printf "墳 %s%%" $VOL
 	fi
 }
 
 printf_network_connection()
 {
+	NETWORK_DEV=$(ip link | grep "^2" | awk '{printf $2}')
 	CONNECT=$(nmcli | grep 'wlan0: ' | awk 'NR==1 {print $2}')
 
 	if [ $CONNECT = "connected" ]
@@ -32,22 +38,22 @@ printf_battery()
 	
 	if [ $STATUS = "Charging" ]
 	then
-		printf "%s%%" $CHARGE
+		printf " %s%%" $CHARGE
 	else
-		printf "%s%%" $CHARGE
+		printf " %s%%" $CHARGE
 	fi
 }
 
 printf_date()
 {
 	# disp year mouth day time
-	printf "%s" "$(date "+%m-%d %H:%M")"
+	printf " %s" "$(date "+%m-%d  %H:%M")"
 }
 
 while true
 do
 	#display network  battery volume date
-	xsetroot -name "$(printf_battery) $(printf_alsa) $(printf_date) $(printf_network_connection) "
+	xsetroot -name "$(print_mem) $(printf_battery) $(printf_alsa) $(printf_date) $(printf_network_connection) "
 
 	sleep 2
 done
